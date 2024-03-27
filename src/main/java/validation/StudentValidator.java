8a606eb05f53d6e9307937872efd79d1ad6d879f
raul-dunca <109088288+raul-dunca@users.noreply.github.com>
2024-03-27 13:14:42 +0200

    Add files via upload

--- src/main/java/validation/StudentValidator.java
+++ src/main/java/validation/StudentValidator.java
@@ -11,16 +11,14 @@ public class StudentValidator implements Validator<Student> {
      */
     @Override
     public void validate(Student entity) throws ValidationException {
-        if(entity.getID().equals("")){
+        if(entity.getID() == null || entity.getID().equals("") ){
             throw new ValidationException("Id incorect!");
         }
-        if(entity.getID() == null){
-            throw new ValidationException("Id incorect!");
-        }
+
         if(entity.getNume() == ""){
             throw new ValidationException("Nume incorect!");
         }
-        if(entity.getGrupa() < 0) {
+        if(entity.getGrupa() < 0 ) {
             throw new ValidationException("Grupa incorecta!");
         }
         if(entity.getEmail() == null){
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -12,6 +12,9 @@ import validation.NotaValidator;
 import validation.StudentValidator;
 import validation.TemaValidator;
 import validation.ValidationException;
+import view.UI;
+
+import static java.lang.Integer.MAX_VALUE;
 
 /**
  * Unit test for simple App.
@@ -46,6 +49,33 @@ public class AppTest
     }
 
 
+    public void testaddStudentGood(){
+
+
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+
+        Student s= new Student("103","Lory",931,"lory@gmail.com") ;
+
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+
+
+
+        Student st1=service.addStudent(s);
+
+        assertEquals(st1.getID(), s.getID());
+        assertEquals(st1.getEmail(), s.getEmail());
+        assertEquals(st1.getNume(), s.getNume());
+        assertEquals(st1.getGrupa(), s.getGrupa());
+    }
+
     public void testaddStudentWithNegativeGroup(){
 
 
@@ -73,9 +103,144 @@ public class AppTest
 
     }
 
+    public void testaddStudentWithMAX_VALUE1Group(){
 
 
-    public void testaddStudentWithInvalidEmail(){
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+
+        Student s= new Student("41","Lory",MAX_VALUE+1,"lory@gmail.com") ;
+
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+
+
+        try {
+            service.addStudent(s);
+            fail("Expected ValidationException for MaxValue");
+        } catch (ValidationException e) {
+            assertEquals("Grupa incorecta!", e.getMessage());
+        }
+
+    }
+
+
+    public void testaddStudentWithMAX_VALUEGroup(){
+
+
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+
+        Student s= new Student("41","Lory",MAX_VALUE,"lory@gmail.com") ;
+
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+
+
+        try {
+            service.addStudent(s);
+
+        } catch (ValidationException e) {
+            fail("Unexpected error thrown!");
+        }
+
+    }
+
+
+    public void testaddStudentWithZeroGroup(){
+
+
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+
+        Student s= new Student("41321","Lory",0,"lory@gmail.com") ;
+
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+
+
+        try {
+            service.addStudent(s);
+
+        } catch (ValidationException e) {
+            fail("Unexpected error thrown!");
+        }
+
+    }
+
+    public void testaddStudentWithNullId(){
+
+
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+
+        Student s= new Student(null,"Lory",931,"a") ;
+
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+
+        try {
+            service.addStudent(s);
+            fail("Expected ValidationException for null id");
+        } catch (ValidationException e) {
+            assertEquals("Id incorect!", e.getMessage());
+        }
+
+    }
+
+
+    public void testaddStudentWithEmptyId(){
+
+
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+
+        Student s= new Student("","Lory",931,"a") ;
+
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+
+        try {
+            service.addStudent(s);
+            fail("Expected ValidationException for empty id");
+        } catch (ValidationException e) {
+            assertEquals("Id incorect!", e.getMessage());
+        }
+
+    }
+
+
+    public void testaddStudentWithNullEmail(){
 
 
         StudentValidator studentValidator = new StudentValidator();
@@ -94,9 +259,90 @@ public class AppTest
 
         try {
             service.addStudent(s);
-            fail("Expected ValidationException for negative group");
+            fail("Expected ValidationException for null  email");
         } catch (ValidationException e) {
             assertEquals("Email incorect!", e.getMessage());
+        }
+
+    }
+
+
+    public void testaddStudentWithEmptyEmail(){
+
+
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+
+        Student s= new Student("199","Lory",931,"") ;
+
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+
+        try {
+            service.addStudent(s);
+            fail("Expected ValidationException for empty email");
+        } catch (ValidationException e) {
+            assertEquals("Email incorect!", e.getMessage());
+        }
+
+    }
+
+
+    public void testaddStudentWithNullName(){
+
+
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+
+        Student s= new Student("199",null,931,"da") ;
+
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+
+        try {
+            service.addStudent(s);
+            fail("Expected ValidationException for null name");
+        } catch (ValidationException e) {
+            assertEquals("Nume incorect!", e.getMessage());
+        }
+
+    }
+
+
+    public void testaddStudentWithEmptyName(){
+
+
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+
+        Student s= new Student("199","",931,"da") ;
+
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+
+        try {
+            service.addStudent(s);
+            fail("Expected ValidationException for empty name");
+        } catch (ValidationException e) {
+            assertEquals("Nume incorect!", e.getMessage());
         }
 
     }
